3aa8ebe4f368c35ba2eda46d8726213b5c26dac0
Caue SJ <caue.silvaa@gmail.com>
2021-11-07 11:43:02 -0300

    Remove test button Expense Item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,20 +3,13 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-const ExpenseItem = (props) => {  
-  const [title, setTitle] = useState(props.title);
-  
-  const clickHandler = () => {
-    setTitle('New Title Here!');
-  };
-
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Click me!</button>
       </div>
     </Card>
   );
